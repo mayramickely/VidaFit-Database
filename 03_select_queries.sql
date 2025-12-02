6cf9a357d53e6659c7dcd69b461e0bcf9c1bf7a9
Mayra Mickely <mickelyp@gmail.com>
2025-12-01 21:22:05 -0300

    Add files via upload

--- 03_select_queries.sql
+++ 03_select_queries.sql
@@ -1,13 +1,1 @@
-SELECT Nome, Email, Telefone FROM Aluno ORDER BY Nome ASC;
-SELECT A.Modalidade, A.Horario, I.Nome AS Instrutor
-FROM Aula A JOIN Instrutor I ON A.InstrutorID = I.InstrutorID;
-SELECT Al.Nome, Au.Modalidade
-FROM InscricaoAula IA
-JOIN Aluno Al ON IA.AlunoID = Al.AlunoID
-JOIN Aula Au ON IA.AulaID = Au.AulaID
-WHERE Au.Modalidade = 'Pilates';
-SELECT Al.Nome, P.Valor, P.Status
-FROM Pagamento P
-JOIN Aluno Al ON P.AlunoID = Al.AlunoID
-WHERE Status = 'Pendente';
-SELECT Nome, CPF FROM Aluno LIMIT 2;
+-- SELECT QUERIES SCRIPT
--- 04_update_delete.sql
+++ 04_update_delete.sql
@@ -1,6 +1,1 @@
-UPDATE Aluno SET Telefone='11900001111' WHERE AlunoID=1;
-UPDATE Pagamento SET Status='Pago' WHERE PagamentoID=3;
-UPDATE Aula SET Capacidade=30 WHERE AulaID=2;
-DELETE FROM Checkin WHERE CheckinID=1;
-DELETE FROM InscricaoAula WHERE InscricaoID=3;
-DELETE FROM Aluno WHERE AlunoID=3;
+-- UPDATE/DELETE SCRIPT
